cdc3c52b8bab5110e619b364d075f6cf8fe21f02
Alan Plum <me@pluma.io>
2014-07-25 16:27:04 +0200

    Initial commit.

---
+++ mailer.js
@@ -0,0 +1,48 @@
+/*jshint indent: 2, nomen: true, maxlen: 120 */
+/*global require, exports, applicationContext */
+var queues = require('org/arangodb/foxx').queues,
+  internal = require('internal');
+
+queues.registerTask(applicationContext.configuration.taskName, {
+  maxFailures: applicationContext.configuration.maxFailures,
+  execute: function (data) {
+    'use strict';
+    var response, body;
+    response = internal.download(
+      'http://api.postmarkapp.com/email',
+      JSON.stringify(data),
+      {
+        method: 'POST',
+        headers: {
+          'accept': 'application/json',
+          'content-type': 'application/json',
+          'x-postmark-server-token': applicationContext.configuration.apiKey
+        }
+      }
+    );
+    if (!response.body) {
+      if (Math.floor(response.code / 100) === 2) {
+        return;
+      }
+      throw new Error('Server sent an empty response with status ' + response.code);
+    }
+    body = JSON.parse(response.body);
+    if (body.ErrorCode !== 0) {
+      throw new Error(
+        'Server returned error code ' +
+        body.ErrorCode +
+        ' with message: ' +
+        body.Message
+      );
+    }
+  }
+});
+
+Object.defineProperty(exports, 'taskName', {
+  get: function () {
+    'use strict';
+    return applicationContext.configuration.taskName;
+  },
+  configurable: false,
+  enumerable: true
+});
